570b22c43af711190724ab1956b4392569a0de13
AkshayKhot07 <akshaykhot743@gmail.com>
2022-08-24 20:32:17 +0530

    Article section refactored

--- src/pages/article/Article.js
+++ src/pages/article/Article.js
@@ -8,6 +8,22 @@ export default function Article() {
         Space to pen down my thoughts on software development, work experiences
         and much more.
       </p>
+
+      <div className="articles-2022-container">
+        <h2 className="articles-2022-title">2022</h2>
+        <div className="all-articles-2022">
+          <div className="article-2022-one">
+            <a
+              href={`https://dev.to/akshaykhot07/creating-an-accordion-using-vanilla-javascript-3o55`}
+              className="article-2022"
+              target="_blank"
+            >
+              Creating an Accordion using vanilla Javascript
+            </a>
+            <span className="article-date">24'Aug</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
